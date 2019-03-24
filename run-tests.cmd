9e847e5db18021fa6fa3ca0cae5fca1ddaf76d73
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-03-24 21:11:52 +0000

    Try to make Python3 work on appveyor

--- run-tests.cmd
+++ run-tests.cmd
@@ -1,12 +1,12 @@
-python37 wiremock/wiremock-wrapper.py start 41375
-python37 wiremock/wiremock-wrapper.py start 8222
-python37 broker/activemq-wrapper.py start
+python wiremock/wiremock-wrapper.py start 41375
+python wiremock/wiremock-wrapper.py start 8222
+python broker/activemq-wrapper.py start
 
 nunit3-console test/specs/bin/Release/TDL.Test.Specs.dll
 SET TestResult=%ERRORLEVEL%
 
-python37 wiremock/wiremock-wrapper.py stop 41375
-python37 wiremock/wiremock-wrapper.py stop 8222
-python37 broker/activemq-wrapper.py stop
+python wiremock/wiremock-wrapper.py stop 41375
+python wiremock/wiremock-wrapper.py stop 8222
+python broker/activemq-wrapper.py stop
 
 EXIT %TestResult%
